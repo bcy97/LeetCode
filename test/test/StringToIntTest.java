f2544394fd27c88f69ddbd604ea56d1f5adf44b6
151250002 <151250002@smail.nju.edu.cn>
2017-03-24 11:33:06 +0800

    8String to Interger

---
+++ test/test/StringToIntTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.Test;
+
+import code.StringToInt;
+
+import static org.junit.Assert.*;
+
+public class StringToIntTest {
+
+	@Test
+	public void testStringToInt1(){
+		StringToInt stringToInt = new StringToInt();
+		assertEquals(0, stringToInt.stringToInt("        "));
+	}
+	@Test
+	public void testStringToInt2(){
+		StringToInt stringToInt = new StringToInt();
+		assertEquals(12345678, stringToInt.stringToInt("12345678"));
+	}
+	@Test
+	public void testStringToInt3(){
+		StringToInt stringToInt = new StringToInt();
+		assertEquals(-123, stringToInt.stringToInt("   -123aweoifh"));
+	}
+	@Test
+	public void testStringToInt4(){
+		StringToInt stringToInt = new StringToInt();
+		assertEquals(Integer.MAX_VALUE, stringToInt.stringToInt("987654324335465678789"));
+	}
+	@Test
+	public void testStringToInt5(){
+		StringToInt stringToInt = new StringToInt();
+		assertEquals(Integer.MIN_VALUE, stringToInt.stringToInt("-2147483648"));
+	}
+}
