aafda0b233ff492b2ec9e6194e09b7f5fe9796ac
bcy97 <609233559@qq.com>
2018-05-14 15:44:42 +0800

    add 88. Merge Sorted Array,add a method beats 99.91%

--- src/code/Array/MergeSortedArray_88.java
+++ src/code/Array/MergeSortedArray_88.java
@@ -5,7 +5,12 @@ import java.util.Arrays;
 public class MergeSortedArray_88 {
 
     public static void main(String[] args) {
-
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        merge(nums1, 0, nums2, 1);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
     }
 
     public void merge1(int[] nums1, int m, int[] nums2, int n) {
@@ -13,10 +18,20 @@ public class MergeSortedArray_88 {
         Arrays.sort(nums1);
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    /**
+     * beat 99.91%
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1, j = n - 1, k = m + n - 1;
         while (i >= 0 && j >= 0 && k >= 0) {
-            
+            nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+        while (j >= 0) {
+            nums1[j] = nums2[j--];
         }
     }
 }
