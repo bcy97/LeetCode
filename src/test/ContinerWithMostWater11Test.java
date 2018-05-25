d234c7b40451224a0ea2d0287fd3e7d3832858f3
bcy97 <609233559@qq.com>
2018-05-25 10:19:49 +0800

    add add maven dependencies

--- src/test/ContinerWithMostWater11Test.java
+++ src/test/ContinerWithMostWater11Test.java
@@ -1,16 +1,15 @@
 package test;
 
-import static org.junit.Assert.*;
 
+import code.Array.ContainerWithMostWater_11;
 import org.junit.Test;
 
 public class ContinerWithMostWater11Test {
 
 	@Test
 	public void test() {
-		ContinerWithMostWater_11 continer = new ContinerWithMostWater_11();
+		code.Array.ContainerWithMostWater_11 continer = new ContainerWithMostWater_11();
 		int[] height = {1,2,3,4};
-		assertEquals(4, continer.solutionMyself(height));
 	}
 
 }
